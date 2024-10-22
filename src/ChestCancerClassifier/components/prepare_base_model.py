0873e9ea04b98e93502522e753e09be0eae16dcb
noblejemini <noblejemini101@gmail.com>
2024-10-22 12:32:50 +0530

    resnet

--- src/ChestCancerClassifier/components/prepare_base_model.py
+++ src/ChestCancerClassifier/components/prepare_base_model.py
@@ -11,7 +11,7 @@ class PrepareBaseModel:
 
     
     def get_base_model(self):
-        self.model = tf.keras.applications.vgg16.VGG16(
+        self.model = tf.keras.applications.ResNet50(
             input_shape=self.config.params_image_size,
             weights=self.config.params_weights,
             include_top=self.config.params_include_top
